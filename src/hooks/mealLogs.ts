5259371627effde3b46a6b192e63e7bc8025c454
Glenn <glennchiang24@gmail.com>
2026-06-15 17:40:00 +0800

    Add mutations for meal logs

--- src/hooks/mealLogs.ts
+++ src/hooks/mealLogs.ts
@@ -1,16 +1,47 @@
 import { mealLogApi } from "@/api/mealLogs";
-import { useQuery } from "@tanstack/react-query";
+import { MealLogPost } from "@/types";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 
-const useGetMealLogs = () => {
+export const useGetMealLogs = () => {
   return useQuery({
     queryKey: ["mealLogs"],
     queryFn: mealLogApi.getMyMealLogs,
   });
 };
 
-const useGetMealLog = (mealLogId: number) => {
+export const useGetMealLog = (mealLogId: number) => {
   return useQuery({
     queryKey: ["mealLogs", mealLogId],
     queryFn: () => mealLogApi.getMealLog(mealLogId),
+  });
+};
+
+export const useCreateMealLog = (data: MealLogPost) => {
+  const queryClient = new QueryClient();
+  return useMutation({
+    mutationFn: () => mealLogApi.createMealLog(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["meal-logs"] });
+    },
+  });
+};
+
+export const useUpdateMealLog = (meallogId: number, data: MealLogPost) => {
+  const queryClient = new QueryClient();
+  return useMutation({
+    mutationFn: () => mealLogApi.updateMealLog(meallogId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["meal-logs", meallogId] });
+    },
+  });
+};
+
+export const useDeleteMealLog = (meallogId: number) => {
+  const queryClient = new QueryClient();
+  return useMutation({
+    mutationFn: () => mealLogApi.deleteMealLog(meallogId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["meal-logs"] });
+    },
   });
 };
