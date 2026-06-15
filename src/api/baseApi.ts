e60c01a1b17652941597cb58d270b4f30edf0493
Glenn <glennchiang24@gmail.com>
2026-06-15 16:53:47 +0800

    Modify baseApi to send cookies

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -4,7 +4,8 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json"
-  }
+  },
+  withCredentials: true
 })
 
 // TODO: Attach token using interceptors
